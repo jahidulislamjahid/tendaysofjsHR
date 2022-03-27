fcc98aaf772bed21b42a5eb7a67272e1f9a0e688
jahid <info.jahidulislamjahid@gmail.com>
2022-03-27 15:32:54 +0600

    hello world

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,7 +1,15 @@
-let parameterVariable = "Welcome to 10 Days of JavaScript!";
-function greeting(parameterVariable) {
-    console.log('Hello, World!');
-    console.log(parameterVariable);
+// let parameterVariable = "Welcome to 10 Days of JavaScript!";
+// function greeting(parameterVariable) {
+//     console.log('Hello, World!');
+//     console.log(parameterVariable);
     
+// }
+// greeting(parameterVariable);
+
+let parameterVariable = "Welcome to 10 Days of JavaScript!";
+
+let greeting = parameterVariable =>{
+    console.log('Hello, World!');
+    console.log(parameterVariable);   
 }
 greeting(parameterVariable);
