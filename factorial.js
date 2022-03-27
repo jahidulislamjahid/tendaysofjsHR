8b29ae25de0d286f072ceb8495b47197d563a77a
jahid <info.jahidulislamjahid@gmail.com>
2022-03-27 16:41:24 +0600

    factorial

--- factorial.js
+++ factorial.js
@@ -8,6 +8,19 @@
 // console.log(factorial(4));
 
 
+// let factorial = n => {
+//     let f = 1;
+//     while (n >= 1) {
+//         f = f * n;
+//         n = n - 1;
+//     }
+//     return f;
+// }
+// let ans = 4;
+
+// console.log(factorial(ans));
+
+
 let factorial = (n)=> {
     let ans = 1;
     for (var i = 1; i<=n; i++){
