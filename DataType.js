d941aa49e7a99211620ce409c84380716696442a
jahid <info.jahidulislamjahid@gmail.com>
2022-03-27 15:54:31 +0600

    DataType

--- DataType.js
+++ DataType.js
@@ -1,7 +1,22 @@
 let secondInteger = 12;
 let secondDecimal = 4.32;
 let secondString = 'is the best place to learn and practice coding!';
-function performOperation(secondInteger, secondDecimal, secondString) {
+// function performOperation(secondInteger, secondDecimal, secondString) {
+//     const firstInteger = 4;
+    
+//     const firstDecimal = 4.0;
+    
+//     const firstString = 'HackerRank ';
+    
+//     console.log(parseInt(secondInteger)+firstInteger);
+//     console.log(parseFloat(secondDecimal)+firstDecimal);
+//     console.log(firstString+secondString);
+      
+// }
+
+// performOperation(secondInteger,secondDecimal,secondString);
+
+let performOperation = (secondInteger, secondDecimal, secondString) => {
     const firstInteger = 4;
     
     const firstDecimal = 4.0;
