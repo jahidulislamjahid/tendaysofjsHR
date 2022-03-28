d9988d76f6b5aeda0e04ef54c86c99747155bf79
jahid <info.jahidulislamjahid@gmail.com>
2022-03-28 15:12:42 +0600

    let and constant

---
+++ LetandConstant.js
@@ -0,0 +1,11 @@
+
+
+let main = () => {
+    const PI = Math.PI;
+    let r = 2.6;
+    let area = PI*r*r;
+    let perimeter = 2*PI*r;
+    console.log(area);
+    console.log(perimeter);
+}   
+main();
